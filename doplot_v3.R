9c4a82ca8769f2a5db3aa33c02929c32745479ed
Steve Powers <st3powers@gmail.com>
2019-01-06 19:57:30 -0800

    adding comments, deleting legacy R code

--- doplot_v3.R
+++ doplot_v3.R
@@ -9,8 +9,7 @@ base_dir<-getwd()
 setwd(base_dir)
 
 # ==================
-# = Load Libraries
-#function for installing packages first time only
+# function for installing & loading packages (installs only if not already installed)
 usePackage <- function(p) 
 {
   if (!is.element(p, installed.packages()[,1]))
@@ -18,73 +17,59 @@ usePackage <- function(p)
   require(p, character.only = TRUE)
 }
 
-#packages needed
-packagesCRAN<-c("plyr","dplyr","devtools","reshape","tidyr","ggthemes","RColorBrewer","scales",
+# function that converts any open/close date to the corresponding day of year (doy= number of days since Jan 1st)
+#doy<-function(data){
+#  if(data$closed_mo==11){
+#    closed_doy<-data$closed_day}
+#  if(data$closed_mo==12){
+#    closed_doy<-data$closed_day+30}
+#  if(data$closed_mo==1){
+#    closed_doy<-data$closed_day+30+31}
+#  if(data$closed_mo==2){
+#    closed_doy<-data$closed_day+30+31+31}
+#  if(data$closed_mo==3){
+#    closed_doy<-data$closed_day+30+31+31+28}
+#  if(data$closed_mo==4){
+#    closed_doy<-data$closed_day+30+31+31+28+31}
+#  if(data$opened_mo==11){
+#    opened_doy<-data$opened_day}
+#  if(data$opened_mo==12){
+#    opened_doy<-data$opened_day+30}
+#  if(data$opened_mo==1){
+#    opened_doy<-data$opened_day+30+31}
+#  if(data$opened_mo==2){
+#    opened_doy<-data$opened_day+30+31+31}
+#  if(data$opened_mo==3){
+#    opened_doy<-data$opened_day+30+31+31+28}
+#  if(data$opened_mo==4){
+#    opened_doy<-data$opened_day+30+31+31+28+31}
+#  if(data$opened_mo==5){
+#    opened_doy<-data$opened_day+30+31+31+28+31+30}  
+#  df<-data.frame(closed_doy,opened_doy)
+#  return(df)
+#}
+
+
+# packages to install & load
+packagesCRAN<-c("ggplot2","plyr","dplyr","devtools","reshape","tidyr","ggthemes","RColorBrewer","scales",
                 "tidyverse","lubridate","viridis","png")
 
-#packagesCRAN<-c("plyr","dplyr","devtools","reshape","tidyr","ggthemes","RColorBrewer","scales",
-#                "tidyverse","forcats","lubridate","viridis","gifski","png")
-
-#iterate usePackage function across package names
+# iterate usePackage function across package names
 lapply(packagesCRAN,FUN=usePackage)
 
-#additional finicky packages, handle separately
-
-library("ggplot2")
+# additional finicky packages, install and load separately
+#library("ggplot2")
 
 #install_github("dgrtwo/gganimate", force=TRUE)
 #install_github("thomasp85/gganimate/releases/tag/v0.1.1", force=TRUE)
 library("gganimate")
 
-ice<-read.csv("data.csv")
-names(ice)[4]<-"ice_duration"
-ice<-ice[-c(1:3),]
-ice$year<-as.numeric(substr(ice$WINTER,1,4))+1
+# read the daterr
+ice<-read.csv("icedata.csv")
 
-doy<-function(data){
-  if(data$closed_mo==11){
-    closed_doy<-data$closed_day
-  }
-  if(data$closed_mo==12){
-    closed_doy<-data$closed_day+30
-  }
-  if(data$closed_mo==1){
-    closed_doy<-data$closed_day+30+31
-  }
-  if(data$closed_mo==2){
-    closed_doy<-data$closed_day+30+31+31
-  }
-  if(data$closed_mo==3){
-    closed_doy<-data$closed_day+30+31+31+28
-  }
-  if(data$closed_mo==4){
-    closed_doy<-data$closed_day+30+31+31+28+31
-  }
-  
-  if(data$opened_mo==11){
-    opened_doy<-data$opened_day
-  }
-  if(data$opened_mo==12){
-    opened_doy<-data$opened_day+30
-  }
-  if(data$opened_mo==1){
-    opened_doy<-data$opened_day+30+31
-  }
-  if(data$opened_mo==2){
-    opened_doy<-data$opened_day+30+31+31
-  }
-  if(data$opened_mo==3){
-    opened_doy<-data$opened_day+30+31+31+28
-  }
-  if(data$opened_mo==4){
-    opened_doy<-data$opened_day+30+31+31+28+31
-  }
-  if(data$opened_mo==5){
-    opened_doy<-data$opened_day+30+31+31+28+31+30
-  }  
-  df<-data.frame(closed_doy,opened_doy)
-  return(df)
-}
+names(ice)[4]<-"ice_duration" # rename this column
+ice<-ice[-c(1:3),] # drop the first few years of record (data gaps)
+ice$year<-as.numeric(substr(ice$WINTER,1,4))+1 # create 'year' column (1996 for northern winter of 1995-1996)
 
 
 closed<-ice %>% select(year,ice_duration,date=CLOSED0) %>% as.data.frame()
@@ -141,31 +126,30 @@ plotdata<-df
 plotdata$txt<-as.character(plotdata$txt)
 
 plotdata<-plotdata[order(plotdata$txt,plotdata$z),]
-#plotdata$colour[which(plotdata$x==0)]<-""
 plotdata$colour<-as.numeric(plotdata$txt2)
 
 
 # workaround to create the 'pause effect' after last plotted point
 # use of end_pause argument (gganimate) produced this error... Error: Additional frame variables must have the same length as the number of frames
 
-plotdata0<-plotdata
+#plotdata0<-plotdata
 pausedata<-subset(plotdata,plotdata$year>=2018)
-rep.pausedata <- function(data=pausedata,n=50,year=2018) 
-{
-  for (i in 1:n){
-    namei<-paste("pausedata",n,sep="")
-    xi<-year+(i*.05/n)
-    pausedata.i<-data
-    pausedata.i$x<-xi
-    assign(namei,pausedata.i)
-  }
-}
-lapply(pausedata,FUN=rep.pausedata)
+#rep.pausedata <- function(data=pausedata,n=50,year=2018) 
+#{
+#  for (i in 1:n){
+#    namei<-paste("pausedata",n,sep="")
+#    xi<-year+(i*.05/n)
+#    pausedata.i<-data
+#    pausedata.i$x<-xi
+#    assign(namei,pausedata.i)
+#  }
+#}
+#lapply(pausedata,FUN=rep.pausedata)
 
 
-rep.pausedata(pausedata)
+#rep.pausedata(pausedata)
 
-pause.n<-50
+pause.n<-75
 pause.year<-2018
 for (i in 1:pause.n){
   print(i)
@@ -178,23 +162,12 @@ for (i in 1:pause.n){
 #  assign(pausedata.i,namei)  
 }
 
-plotdataB<-rbind(plotdata0,pausedataX)
+plotdata.pause<-rbind(plotdata,pausedataX)
 
-#p.magma <- ggplot(plotdataB, aes(x, y,frame=x,shape=event,color=ice_duration)) +
-#  geom_point(size=1.5)+
-#  scale_color_viridis(direction=-1,end=0.8,option="A")+
-#  ylim(-60,150)+
-#  xlab("Year")+ylab("Days since January 1st")+
-#  scale_x_continuous(breaks = scales::pretty_breaks(n = 9))+
-#  scale_y_continuous(breaks = scales::pretty_breaks(n = 8))+
-#  theme_bw()#
+plotdata.pause$ice_duration2<-plotdata.pause$ice_duration
+plotdata.pause$ice_duration2[which(plotdata.pause$ice_duration2<=60)]<-60
 
-#p.cume.magma<-p.magma +  transition_manual(x,cumulative=TRUE)
-
-plotdataB$ice_duration2<-plotdataB$ice_duration
-plotdataB$ice_duration2[which(plotdataB$ice_duration2<=60)]<-60
-
-p1 <- ggplot(plotdataB, aes(x, y,frame=x,shape=event,color=ice_duration)) +
+p1 <- ggplot(plotdata.pause, aes(x, y,frame=x,shape=event,color=ice_duration)) +
   geom_point(size=1.5)+
   ylim(-60,150)+
   xlab("Year")+ylab("Days since January 1st")+
@@ -202,7 +175,7 @@ p1 <- ggplot(plotdataB, aes(x, y,frame=x,shape=event,color=ice_duration)) +
   scale_y_continuous(breaks = scales::pretty_breaks(n = 8))+
   theme_bw()#
 
-p2 <- ggplot(plotdataB, aes(x, y,frame=x,shape=event,color=ice_duration2)) +
+p2 <- ggplot(plotdata.pause, aes(x, y,frame=x,shape=event,color=ice_duration2)) +
   geom_point(size=1.5)+
   ylim(-60,150)+
   xlab("Year")+ylab("Days since January 1st")+
@@ -212,7 +185,6 @@ p2 <- ggplot(plotdataB, aes(x, y,frame=x,shape=event,color=ice_duration2)) +
 
 p.magma<-p1 +  transition_manual(x,cumulative=TRUE)+scale_color_viridis(direction=-1,end=0.8,option="A")
 p.viridis<-p2 +  transition_manual(x,cumulative=TRUE)+scale_color_viridis(direction=-1,option="D")
-
 
 anim_save("ice.magma.gif", p.magma,width=400,height=250)
 anim_save("ice.viridis.gif", p.viridis,width=400,height=250)
