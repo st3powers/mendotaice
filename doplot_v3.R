9c8510403f265a7c0cca470813e8ab0eff1b4fec
Steve Powers <st3powers@gmail.com>
2019-01-06 20:09:18 -0800

    more code cleaning

--- doplot_v3.R
+++ doplot_v3.R
@@ -1,10 +1,10 @@
-# ==========
-# = Clear workspace =
+# ==================
+# Clear workspace
 rm(list = ls()) 
 graphics.off()
 
-# ==========
-# = Set WD as needed by your machine!! 
+# ==================
+# Set WD as needed by your machine
 base_dir<-getwd()
 setwd(base_dir)
 
@@ -17,39 +17,7 @@ usePackage <- function(p)
   require(p, character.only = TRUE)
 }
 
-# function that converts any open/close date to the corresponding day of year (doy= number of days since Jan 1st)
-#doy<-function(data){
-#  if(data$closed_mo==11){
-#    closed_doy<-data$closed_day}
-#  if(data$closed_mo==12){
-#    closed_doy<-data$closed_day+30}
-#  if(data$closed_mo==1){
-#    closed_doy<-data$closed_day+30+31}
-#  if(data$closed_mo==2){
-#    closed_doy<-data$closed_day+30+31+31}
-#  if(data$closed_mo==3){
-#    closed_doy<-data$closed_day+30+31+31+28}
-#  if(data$closed_mo==4){
-#    closed_doy<-data$closed_day+30+31+31+28+31}
-#  if(data$opened_mo==11){
-#    opened_doy<-data$opened_day}
-#  if(data$opened_mo==12){
-#    opened_doy<-data$opened_day+30}
-#  if(data$opened_mo==1){
-#    opened_doy<-data$opened_day+30+31}
-#  if(data$opened_mo==2){
-#    opened_doy<-data$opened_day+30+31+31}
-#  if(data$opened_mo==3){
-#    opened_doy<-data$opened_day+30+31+31+28}
-#  if(data$opened_mo==4){
-#    opened_doy<-data$opened_day+30+31+31+28+31}
-#  if(data$opened_mo==5){
-#    opened_doy<-data$opened_day+30+31+31+28+31+30}  
-#  df<-data.frame(closed_doy,opened_doy)
-#  return(df)
-#}
-
-
+# ==================
 # packages to install & load
 packagesCRAN<-c("ggplot2","plyr","dplyr","devtools","reshape","tidyr","ggthemes","RColorBrewer","scales",
                 "tidyverse","lubridate","viridis","png")
@@ -58,25 +26,26 @@ packagesCRAN<-c("ggplot2","plyr","dplyr","devtools","reshape","tidyr","ggthemes"
 lapply(packagesCRAN,FUN=usePackage)
 
 # additional finicky packages, install and load separately
-#library("ggplot2")
-
 #install_github("dgrtwo/gganimate", force=TRUE)
 #install_github("thomasp85/gganimate/releases/tag/v0.1.1", force=TRUE)
 library("gganimate")
 
+# ==================
 # read the daterr
 ice<-read.csv("icedata.csv")
 
+# ==================
+# prepare the daterr
 names(ice)[4]<-"ice_duration" # rename this column
 ice<-ice[-c(1:3),] # drop the first few years of record (data gaps)
 ice$year<-as.numeric(substr(ice$WINTER,1,4))+1 # create 'year' column (1996 for northern winter of 1995-1996)
-
-
-closed<-ice %>% select(year,ice_duration,date=CLOSED0) %>% as.data.frame()
+closed<-ice %>% select(year,ice_duration,date=CLOSED0) %>% as.data.frame() # slimmer data frame of closed events
 closed$event<-"closed"
-opened<-ice %>% select(year,ice_duration,date=OPENED0) %>% as.data.frame()
+opened<-ice %>% select(year,ice_duration,date=OPENED0) %>% as.data.frame() # slimmer data frame of opened events
 opened$event<-"opened"
-ice2<-rbind(closed,opened)
+ice2<-rbind(closed,opened) # slim data frame of closed + opened events
+
+# create column for day of year (doy= days since Jan 1st)
 ice2$doy<-0
 monthstr<-substr(ice2$date,nchar(as.character(ice2$date))-2,nchar(as.character(ice2$date)))
 dayofmo<-as.numeric(substr(ice2$date,1,nchar(as.character(ice2$date))-4))
@@ -87,8 +56,6 @@ ice2$doy[which(monthstr=="Feb")]<-dayofmo[which(monthstr=="Feb")]+31
 ice2$doy[which(monthstr=="Mar")]<-dayofmo[which(monthstr=="Mar")]+31+28
 ice2$doy[which(monthstr=="Apr")]<-dayofmo[which(monthstr=="Apr")]+31+28+31
 ice2$doy[which(monthstr=="May")]<-dayofmo[which(monthstr=="May")]+31+28+31+30
-
-names(ice2)
 
 ##############################
 datax<-ice2
@@ -106,20 +73,22 @@ for(i in 1:length(unique(datax$z))) {
   assign(namei,data.i)
 }
 
-things<-ls()[grep("datax.*",ls())]
+things<-ls()[grep("datax.*",ls())] # list of datax objects
 
+# combine datax objects
 for(i in 1:length(things)){
   datai<-get(things[i])
   if(i==1){data.out<-datai}
   if(i>1){data.out<-rbind(data.out,datai)}
 }
 rbind<-data.out
+rbind<-datax
 
 rbind<-unique(rbind)
 rbind<-rbind[order(rbind$z),]
-rbind.end<-rbind.end[order(rbind.end$z,rbind.end$z,decreasing=TRUE),]
+#rbind.end<-rbind.end[order(rbind.end$z,rbind.end$z,decreasing=TRUE),]
 
-n.approx<-100
+#n.approx<-100
 
 df<-rbind
 plotdata<-df
@@ -128,28 +97,10 @@ plotdata$txt<-as.character(plotdata$txt)
 plotdata<-plotdata[order(plotdata$txt,plotdata$z),]
 plotdata$colour<-as.numeric(plotdata$txt2)
 
-
 # workaround to create the 'pause effect' after last plotted point
-# use of end_pause argument (gganimate) produced this error... Error: Additional frame variables must have the same length as the number of frames
-
-#plotdata0<-plotdata
+# because use of end_pause argument (gganimate) produced this error... Error: Additional frame variables must have the same length as the number of frames
 pausedata<-subset(plotdata,plotdata$year>=2018)
-#rep.pausedata <- function(data=pausedata,n=50,year=2018) 
-#{
-#  for (i in 1:n){
-#    namei<-paste("pausedata",n,sep="")
-#    xi<-year+(i*.05/n)
-#    pausedata.i<-data
-#    pausedata.i$x<-xi
-#    assign(namei,pausedata.i)
-#  }
-#}
-#lapply(pausedata,FUN=rep.pausedata)
-
-
-#rep.pausedata(pausedata)
-
-pause.n<-75
+pause.n<-100
 pause.year<-2018
 for (i in 1:pause.n){
   print(i)
